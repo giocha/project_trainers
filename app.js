3c9ccaa83c1f0a5693d714671cacf053a46a75cc
Giorgos Chatzitheodoridis <chatzitheodoridis@outlook.com>
2021-06-27 13:38:01 +0300

    app main initial

---
+++ app.js
@@ -0,0 +1,36 @@
+// ! Requires (Start)
+const express = require('express')
+const morgan = require('morgan')
+const path = require('path')
+const _ = require('debug')('_ route_'+path.basename(__filename,path.extname(__filename)) + ' =>   ')
+
+// ? Routers (Start)
+const indexRouter = require('./routes/index')
+const trainerRouter = require('./routes/trainer')
+
+// ? Routers (End)
+
+// ! Requires (End)
+
+const app = express()
+
+// Set up
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+app.use(morgan('tiny'))
+app.set(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(express.static(path.join(__dirname, 'client')))
+
+// ! Urls (Start)
+app.use('/', indexRouter)
+app.use('/trainers', trainerRouter)
+
+// ! Urls (End)
+
+// Server on port
+const port = process.env.PORT || 3005
+app.listen(port,
+	 err => err 
+	? `Problem on port ${err}`
+	:  _(`Server running on ${port} port`))
