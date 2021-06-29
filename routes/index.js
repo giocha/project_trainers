db790dcbefd12838eefec9b70d3c27c6cb27e838
Giorgos Chatzitheodoridis <chatzitheodoridis@outlook.com>
2021-06-29 23:31:09 +0300

    add trainer initial

--- routes/index.js
+++ routes/index.js
@@ -13,20 +13,21 @@ const trainer = new Trainer()
 let storeId;
 let editCurrentTrainer;
 
+// Show all trainers .
 indexRouter.get('/',(req, res) =>{
 
 		allTrainers()().then(result => {
 			res.render('index', {title : 'Trainers', trainer_ : result.result })
 		})
 })
-
+// Delete selected trainer.
 indexRouter.get('/delete/:trainer_id', (req, res) => {
 
 	deleteTrainer(req.params.trainer_id)().then(() => {
 		res.redirect('/')
 	})
 })
-
+// Edit selected trainer.
 indexRouter.get('/edit/:trainer_id', (req, res) => {
 
 	storeId = req.params.trainer_id
@@ -39,7 +40,7 @@ indexRouter.get('/edit/:trainer_id', (req, res) => {
 	res.render('edit_trainers',{title: 'Update Trainer', trainer_ : allTogether, single : editCurrentTrainer})
 	})
 })
-
+// Post Updated trainer.
 indexRouter.post('/edit', (req, res) => {
 
 	trainer.setFirstName = req.body.fname
@@ -52,6 +53,23 @@ indexRouter.post('/edit', (req, res) => {
 			})
 		}else res.render('error')
 })
+
+// Show trainers list at `/add`.
+indexRouter.get('/add', (req, res) => {
+
+	allTrainers()().then(result => {
+
+		res.render('add_trainer',{title : 'New Trainer', trainer_ : result.result})
+	})
+})
+// Post a new trainer.
+indexRouter.post('/', (req, res) => {
+
+	insertTrainer([req.body.fname, req.body.lname, req.body.sub])().then(result => {
+		res.redirect('/')
+	})
+})
+
 
 module.exports = indexRouter
 
